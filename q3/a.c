55fe11d823119af49d69c5ced887e397dd4408be
Pushpendra Vishwakarma <Pushpendra.hpx2001@gmail.com>
2020-10-24 00:08:07 +0530

    Done 100%

---
+++ q3/a.c
@@ -0,0 +1,136 @@
+/*
+[ Final Correct ]
+    U19EE003 - Pushpendra Vishwakarma
+    Electrical Engineering Department, SVNIT  
+*/
+#include <stdio.h>
+#include <stdlib.h>
+struct Node{
+    int data;
+    struct Node* next;
+}*head;
+
+
+struct Node* createNewNode(int data){
+    struct Node* temp = (struct Node*) malloc(sizeof(struct Node));
+    temp->data = data;
+    temp->next = NULL;
+    return temp;
+}
+void seeList(){
+    struct Node* temp = head;
+    while(temp){
+        printf(" %d ",temp->data);
+        if(!(temp->next == NULL)) printf(" => ");
+        temp = temp->next;
+    }
+}
+void search(){
+    int num;
+    printf("\n Enter a Number to Search :- "); scanf("%d",&num);
+    int count = 0;
+    if(head == NULL){
+        printf("\n Linked List is Already Empty \n");
+        getchar();
+        getchar();
+    }else{
+        struct Node *temp = head;
+        while(temp){
+            count++;
+            if(temp->data == num){
+                printf("\nElement Found at Node No. %d, having data %d\n",count,num);
+                getchar();
+                getchar();
+                return;
+            }
+            temp = temp->next;
+        }
+        printf("\n Element Not Found \n");
+
+                getchar();
+                getchar();
+    }
+}
+void insert(){
+    int num;
+    printf("\n Enter a Number to add in Node :- "); scanf("%d",&num);
+    if(head == NULL){
+        head = createNewNode(num);
+    }else{
+        struct Node *temp = head;
+        while(temp){
+            if(temp->next == NULL){
+                temp->next = createNewNode(num);
+                printf("\n Element Added \n");
+                getchar();
+                getchar();
+                break;
+            }
+            temp = temp->next;
+        }
+    }
+}
+void delete(){
+    if(head==NULL){
+        printf("\n Linked List Already Empty \n");
+        getchar();
+        getchar();
+    }else{
+        struct Node* temp = head;
+        head = head->next;
+        free(temp);
+        getchar();
+        getchar();
+        printf("\n Deleted Head \n");
+    }
+}
+void _DELETE_BY_DEFAULT_(){
+    printf("\n DELETING ALL NODES \n");
+    struct Node* temp = head;
+    while(temp){
+        head = temp;
+        printf("\n Deleted Node having value %d",temp->data);
+        temp = temp->next;
+        free(head);
+    }
+    printf("\n Deleted All Nodes in Linked List \n");
+}
+void end_now(){
+    _DELETE_BY_DEFAULT_();
+    printf("\n ============== Bye Bye ============== \n");
+    exit(0);
+}
+
+void reverse(){
+    struct Node *temp = head;
+    for( temp = head;temp; temp = temp->next){
+        for(struct Node* temp2 = temp->next; temp2; temp2 = temp2->next){
+            if(temp->data < temp2->data){
+                int tmp = temp2->data;
+                temp2->data = temp->data;
+                temp->data = tmp;
+                
+            }
+        }
+    }
+}
+
+int main(){
+    head = NULL;
+    int choice = 0;
+
+    start : 
+    printf("=============================================================================\n");
+    printf("[ Linked List ] - *head =>"); seeList(); printf(" => NULL ");
+    printf("\n\t---- Main Menu ----\n\t1. Search an Element \t[ Press 1 ]\n\t2. Insert an Element \t[ Press 2 ]\n\t3. Delete an Element \t[ Press 3 ]\n\t4. Exit \t\t[ Press 4 ]\n\t5. Reverse \t\t[ Press 5 ]\n============================\nEnter Your Choice : ");
+    scanf("%d",&choice);
+    switch (choice)
+    {
+        case 1: search(); goto start;
+        case 2: insert(); goto start;
+        case 3: delete(); goto start;
+        case 4: end_now(); goto start;
+        case 5: reverse(); goto start;
+        default: printf("\n Entered Wrong Key \n"); goto start;
+    }
+}
---
+++ q3/d.c
@@ -0,0 +1,135 @@
+/*
+[Correct]
+    U19EE003 - Pushpendra Vishwakarma
+    Electrical Engineering Department, SVNIT  
+
+    My Experience with Sentinel Node was that, it really Helped and Simplified my Side Constraints, I dont have to think about the edge cases like about NULL Pointer, at the end will point NULL.
+*/
+#include <stdio.h>
+#include <stdlib.h>
+struct Node{
+    int data;
+    struct Node* next;
+    struct Node* prev;
+}*head;
+
+
+struct Node* createNewNode(int data,struct Node* previous_Node){
+    struct Node* temp = (struct Node*) malloc(sizeof(struct Node));
+    temp->data = data;
+    temp->prev = previous_Node;
+    temp->next = head;
+    return temp;
+}
+void seeList(){
+
+    printf("\n\033[0;31m"); //Set the text to the color red
+    printf(" Head address = %p, head->prev = %p, head->next = %p",head,head->prev,head->next);
+    struct Node *temp = head->next;
+    while(temp != head){
+        printf("\n Value=%d, This address = %p, Prev Address = %p, Next Address = %p => ", temp->data, temp, temp->prev, temp->next);
+        temp = temp->next;
+    }
+
+    printf("\033[0m"); //Resets the text to default color
+}
+void search(){
+    int num;
+    printf("\n Enter a Number to Search :- "); scanf("%d",&num);
+    int count = 0;
+    
+        struct Node *temp = head->next;
+        do{
+            count++;
+            if(temp->data == num){
+                printf("\nElement Found at Node No. %d, having data %d\n",count,num);
+                getchar();
+                getchar();
+                return;
+            }
+            temp = temp->next;
+        }while(temp != head);
+        printf("\n Element Not Found \n");
+
+                getchar();
+                getchar();
+    
+}
+void insert(){
+    int num;
+    printf("\n Enter a Number to add in Node :- "); scanf("%d",&num);
+   
+        struct Node *temp = head->next;
+        while(temp){
+            if(temp->next == head){
+                
+                temp->next = createNewNode(num,temp);
+                (temp->next)->prev = temp;
+                (temp->next)->next = head;
+                head->prev = temp->next;
+                printf("\n Element Added \n");
+                getchar();
+                getchar();
+                break;
+            }
+            temp = temp->next;
+        }
+}
+void delete(){
+    
+        struct Node* temp = head->next;
+        head->next = temp->next;
+        if(head->next != head){
+            free(temp);
+
+            getchar();
+            getchar();
+            printf("\n Deleted Head \n");
+            return;
+        }
+        printf("\n Linked List Already Empty\n");
+        
+    
+}
+void _DELETE_BY_DEFAULT_(){
+    printf("\n DELETING ALL NODES \n");
+    struct Node* temp = head->next;
+    while(temp != head){
+        head->next = temp->next;
+        printf("\n Deleted Node having value %d",temp->data);
+        
+        free(temp);
+        temp = head->next;
+    }
+    printf("\n Deleted All Nodes in Linked List \n");
+}
+void end_now(){
+    _DELETE_BY_DEFAULT_();
+    printf("\n ============== Bye Bye ============== \n");
+    exit(0);
+}
+int main(){
+    struct Node *abcd = (struct Node*) malloc(sizeof(struct Node));
+    abcd->data = 12;
+    abcd->prev = abcd;
+    abcd->next = abcd;
+    
+    // As Circular so Initial Sentinal Declarations
+    head = abcd;
+    int choice = 0;
+
+    start : 
+    printf("=============================================================================\n");
+    printf("[ Circular Doubly Linked List ] - *head =>"); seeList(); printf(" => head ");
+    printf("\n\t---- Main Menu ----\n\t1. Search an Element \t[ Press 1 ]\n\t2. Insert an Element \t[ Press 2 ]\n\t3. Delete an Element \t[ Press 3 ]\n\t4. Exit \t\t[ Press 4 ]\n\t5. Reverse \t\t[ Press 5 ]\n============================\nEnter Your Choice : ");
+    scanf("%d",&choice);
+    switch (choice)
+    {
+        case 1: search(); goto start;
+        case 2: insert(); goto start;
+        case 3: delete(); goto start;
+        case 4: end_now(); goto start;
+        case 5: reverse(); goto start;
+        default: printf("\n Entered Wrong Key \n"); goto start;
+    }
+}
