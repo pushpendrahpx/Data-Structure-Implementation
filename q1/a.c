5ba16fe9fdfb8e7edf52821cf204c9c1be91ad82
Vinod Kumavat <u19ee027@eed.svnit.ac.in>
2020-10-24 16:47:48 +0530

    updated with insertion method to add node

--- q1/a.c
+++ q1/a.c
@@ -1,43 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
-struct Node{
-    int data;
+
+struct Node
+{
+    int element;
     struct Node *next;
 };
 
-struct Node* createNode(int data){
+struct Node* addNode(int element){
     struct Node *temp = (struct Node*) malloc(sizeof(struct Node));
-    temp->data = data;
+    temp->element = element;
     temp->next = NULL;
     return temp;
 }
-int main(){
+
+int main()
+{
     struct Node *head = NULL;
     struct Node *temp = head;
-    
-    temp = createNode(5);
+
+    int elements[8] = {3, 2, 5, 4, 7, 4, 2};
+
+    temp = addNode(5);
     head = temp;
 
-    temp->next = createNode(3);
-    temp = temp->next;
-    temp->next = createNode(2);
-    temp = temp->next;
-    temp->next = createNode(5);
-    temp = temp->next;
-    temp->next = createNode(4);
-    temp = temp->next;
-    temp->next = createNode(7);
-    temp = temp->next;
-    temp->next = createNode(4);
-    temp = temp->next;
-    temp->next = createNode(2);
-    temp = temp->next;
+    for(int j=0; j<7; j++)
+    {
 
+        temp->next = addNode(elements[j]);
+        temp = temp->next;
+    }
 
     temp = head;
     printf(" HEAD ");
     while(temp){
-        printf(" => %d ",temp->data);
+        printf(" => %d ",temp->element);
         temp = temp->next;
     }
     printf(" => NULL ");
