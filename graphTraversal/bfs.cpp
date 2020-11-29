b158e449f4a4a8c8345e4c00b659e50cf3095b55
psuhepndra <pushpendra.hpx2001@gmail.com>
2020-11-29 19:28:44 +0000

    BFS Done

--- graphTraversal/bfs.cpp
+++ graphTraversal/bfs.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <queue>
 
-#define MAX 100
+using namespace std;
+
+queue<int> myq;
 
 struct AdjacencyNode{
     int addr;
@@ -10,6 +12,7 @@ struct AdjacencyNode{
 
 struct Node{
   int data;
+  bool visited;
   struct AdjacencyNode *head;
 };
 void printAdjacencyList(struct Node graph){
@@ -17,11 +20,11 @@ void printAdjacencyList(struct Node graph){
 
     while(tmp){
 
-        printf(" -->> (%d),",tmp->addr);
+        cout<<"-->>,"<<tmp->addr;
 
         tmp = tmp->next;
     }
-    printf(" => NULL ");
+    cout<<" => NULL ";
 }
 void makeEdge(struct Node graph[], int from, int to, int graphSize = 10){
     for(int i = 0; i < graphSize; i++){
@@ -51,14 +54,72 @@ void makeEdge(struct Node graph[], int from, int to, int graphSize = 10){
         }
     }
 }
-using namespace std;
+
+int getIndex(struct Node graph[],int val){
+    for(int i = 0; i < 10; i++){
+        if(val == graph[i].data){
+            return i;
+        }
+    }
+    return -1;
+}
+void traverse(struct Node graph[],int nodeIndex){
+
+  if(nodeIndex >= 0){
+        myq.push(graph[nodeIndex].data);
+        // added initial Vertex
+
+
+    graph[nodeIndex].visited = true;
+
+    int index = getIndex(graph,myq.front());
+    // int index = 0;
+    while(!myq.empty()){
+
+
+        index = getIndex(graph,myq.front());
+        struct AdjacencyNode *tmp = graph[index].head;
+
+        while(tmp){
+
+            if(graph[getIndex(graph,tmp->addr)].visited == false){
+                myq.push(tmp->addr);
+                graph[getIndex(graph,tmp->addr)].visited = true;
+            }
+
+            tmp = tmp->next;
+        }
+
+        cout<<myq.front()<<", ";
+
+
+        myq.pop();
+
+    }
+
+
+
+
+
+
+    /////////////////////////////////////////////////////
+
+
+
+    // cout<<graph[nodeIndex].data <<" - ";
+
+  }
+
+
+}
 int main(){
     struct Node graph[10];
     int arr[10] = {9,4,2,6,3,5,1,8,7,0};
     for(int i = 0; i < 10; i++){
         graph[i].data = arr[i];
         graph[i].head = NULL;
-    } // to initialize graph
+        graph[i].visited = false;
+    } // to initialize grap   h
 
     int t[] = {0,8,3,0,3,1,3,2,3,4,3,7,3,5,6,4,4,9};
     int f[] = {8,0,0,3,1,3,2,3,4,3,7,3,5,3,4,6,9,4};
@@ -66,17 +127,20 @@ int main(){
     for(int i = 0; i < 18; i++){
         makeEdge(graph,t[i],f[i]);
     }
+    // cout<<myq.front();
+
+    traverse(graph,getIndex(graph,0));
+
+
+    // for(int i = 0; i < 10; i++){
+    //     cout<<graph[i].data<<", ("<<i<<")";
+    //     printAdjacencyList(graph[i]);
+    //     // printf("\n");
+    //     cout<<"\n";
+    // }
 
 
 
-    for(int i = 0; i < 10; i++){
-        printf(" %d(%d) ",graph[i].data,i);
-        printAdjacencyList(graph[i]);
-        printf("\n");
-    }
-
-
-
-    printf("\n");
+    cout<<"\n";
     return 0;
 }
